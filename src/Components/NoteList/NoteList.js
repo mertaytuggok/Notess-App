be436422c1dab068fbb94e4342d505fa2013d9e7
mertaytuggok <mertaytuggok@gmail.com>
2023-01-07 17:22:32 +0300

    Delete and New Note added

--- src/Components/NoteList/NoteList.js
+++ src/Components/NoteList/NoteList.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./NoteList.module.css";
+import { deleteNote } from "../../ReduxSlice/noteSlice";
+import CloseSvg from "../../Asset/Close.svg";
 
 export const NoteList = () => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.todos.items);
+
+  console.log(items);
   return (
     <div className={styles.listCardContainer}>
-      <div className={styles.noteCard}>Note 1</div>
-      <div className={styles.noteCard}>Note 2</div>
-      <div className={styles.noteCard}>Note 3</div>
+      {items.map((item) => (
+        <>
+          {" "}
+          <div
+            key={item.id}
+            style={{ backgroundColor: `${item.color}` }}
+            className={styles.noteCard}
+          >
+            <span> {item.note} </span>
+
+            <button
+              className={styles.deleteButton}
+              onClick={() => dispatch(deleteNote(item.id))}
+            >
+              <img src={CloseSvg} />
+            </button>
+          </div>
+        </>
+      ))}
     </div>
   );
 };
--- src/Components/Textarea/Textarea.js
+++ src/Components/Textarea/Textarea.js
@@ -1,31 +1,66 @@
+import { nanoid } from "@reduxjs/toolkit";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addNewNote } from "../../ReduxSlice/noteSlice";
 import styles from "./Textarea.module.css";
 
 export const Textarea = () => {
-  const [checked, setCkeched] = useState(false);
-
-  const handleCheck = () => {
-    setCkeched(false);
-    setCkeched(!checked);
+  const [note, setNote] = useState("");
+  const [color, setColor] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (note.length >= 5) {
+      dispatch(addNewNote({ id: nanoid(), note, color }));
+      setNote("");
+      setColor("");
+    } else {
+      console.log(note.length);
+    }
   };
+
   return (
     <div className={styles.textareaMain}>
-      <textarea
-        placeholder="Enter your note here..."
-        className={styles.Textarea}
-      />
-      <div className={styles.groupButtons}>
-        <div className={styles.colorButtons}>
-          <button onClick={handleCheck} className={styles.pinkButton} />
-          <button onClick={handleCheck} className={styles.blueVioletButton} />
-          <button onClick={handleCheck} className={styles.yellowButton} />
-          <button onClick={handleCheck} className={styles.blueButton} />
-          <button onClick={handleCheck} className={styles.greenButton} />
+      <form onSubmit={handleSubmit}>
+        <textarea
+          placeholder="Enter your note here..."
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+          className={styles.Textarea}
+        />
+        <div className={styles.groupButtons}>
+          <div className={styles.colorButtons}>
+            <button
+              value={`pink`}
+              onClick={(e) => setColor(e.target.value)}
+              className={styles.pinkButton}
+            />
+            <button
+              value={`blueviolet`}
+              onClick={(e) => setColor(e.target.value)}
+              className={styles.blueVioletButton}
+            />
+            <button
+              value={`yellow`}
+              onClick={(e) => setColor(e.target.value)}
+              className={styles.yellowButton}
+            />
+            <button
+              value={`blue`}
+              onClick={(e) => setColor(e.target.value)}
+              className={styles.blueButton}
+            />
+            <button
+              value={`green`}
+              onClick={(e) => setColor(e.target.value)}
+              className={styles.greenButton}
+            />
+          </div>
+          <div>
+            <button className={styles.addButton}>ADD</button>
+          </div>
         </div>
-        <div>
-          <button className={styles.addButton}>ADD</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
